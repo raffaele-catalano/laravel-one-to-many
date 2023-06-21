dc6720a15f199c24820e7e20ad8cba42229d9f6c
raffaele-catalano <raf.catalano@gmail.com>
2023-06-21 18:54:25 +0200

    edited visualization of type in Show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -17,6 +17,7 @@
         <h5><span class="text-primary">Category:</span> {{ $project->category }}</h5>
         <h5><span class="text-primary">Start Date:</span> {{ $project->start_date }}</h5>
         <h5><span class="text-primary">End Date:</span> {{ $project->end_date }}</h5>
+        <h5><span class="text-primary">Type:</span> <span class="badge text-bg-info text-uppercase">{{ $project->type?->name ?? 'undefined' }}</span></td></h5>
         <h5><span class="text-primary">Status:</span> {{ $project->is_closed ? 'Closed' : 'Ongoing' }}</h5>
         <p>{!! $project->description !!}</p>
     </div>
