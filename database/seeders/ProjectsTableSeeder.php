c607dea12ff5f42e7c23d302472b8dcbca149eaa
raffaele-catalano <raf.catalano@gmail.com>
2023-06-21 17:15:30 +0200

    updated Projects Seeder

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator as Faker;
 
 
@@ -21,6 +22,7 @@ class ProjectsTableSeeder extends Seeder
 
             $new_project = new Project();
 
+            $new_project->type_id = Type::inRandomOrder()->first()->id;
             $new_project->name = $faker->sentence();
             $new_project->description = $faker->text();
             $new_project->category = $faker->word();
